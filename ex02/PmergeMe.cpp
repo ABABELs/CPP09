8fe39d36b77637005d73e1cbbef65f5b9c3a6b1d
Arthur Abel <aabel@k2r3p8.42mulhouse.fr>
2024-02-09 14:17:07 +0100

    go

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: aabel <aabel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 11:58:44 by aabel             #+#    #+#             */
-/*   Updated: 2024/02/09 12:36:47 by aabel            ###   ########.fr       */
+/*   Updated: 2024/02/09 14:12:31 by aabel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,9 @@ void PmergeMevector(char **argv)
         else if (vec[i] > vec[i + 1])
             std::swap(vec[i], vec[i + 1]);
     }
+    PrintVector(vec);
     Mergesort(vec);
     PrintVector(vec);
-    // std::vector<unsigned int> vec2;
-    // for (size_t i = 1; i < vec.size(); i += 2)
-    //     vec2.push_back(vec[i]);
-    // for (size_t i = 1; i < vec.size(); i++)
-    //     vec.erase(vec.begin() + i);
-    // PrintVector(vec); 
-    // PrintVector(vec2);
-    // std::sort(vec2.begin(), vec2.end());
-    // PrintVector(vec2);
 }
 
 void PrintVector(std::vector<unsigned int> vec)
@@ -58,45 +50,61 @@ void PrintVector(std::vector<unsigned int> vec)
     std::cout << std::endl;
 }
 
-void    Mergesort(std::vector<unsigned int> &vec)
+void Mergesort(std::vector<unsigned int> &vec)
 {
     if (vec.size() <= 1)
         return;
-    std::vector<unsigned int> left;
-    std::vector<unsigned int> right;
-    // left.assign(vec.begin(), vec.begin() + vec.size() / 2);
-    // right.assign(vec.begin() + vec.size() / 2, vec.end());
-    for (size_t i = 0; i < vec.size(); i++)
-    {
-        if (i < vec.size() / 2)
-            left.push_back(vec[i]);
-        else if (i >= vec.size() / 2)
-            right.push_back(vec[i]);
-    }
-    Mergesort(left);
-    Mergesort(right);
-    vec.clear();
-    while (left.size() > 0 && right.size() > 0)
-    {
-        if (left.front() <= right.front())
-        {
-            vec.push_back(left.front());
-            left.erase(left.begin());
-        }
-        else
-        {
-            vec.push_back(right.front());
-            right.erase(right.begin());
-        }
-    }
-    while (left.size() > 0)
-    {
-        vec.push_back(left.front());
-        left.erase(left.begin());
-    }
-    while (right.size() > 0)
-    {
-        vec.push_back(right.front());
-        right.erase(right.begin());
-    }
+    
+    std::vector<unsigned int> res;
+    for (size_t i = 1; i < vec.size(); i += 2)
+        res.push_back(vec[i - 1]);
+    std::cout << "res: ";
+    PrintVector(res);
+    std::cout << "vec: ";
+    PrintVector(vec);
+    Mergesort(res);
+    
+    vec = res;
 }
+
+// void    Mergesort(std::vector<unsigned int> &vec)
+// {
+    // if (vec.size() <= 1)
+    //     return;
+    // std::vector<unsigned int> left;
+    // std::vector<unsigned int> right;
+    // for (size_t i = 0; i < vec.size(); i++)
+    // {
+    //     if (i < vec.size() / 2)
+    //         left.push_back(vec[i]);
+    //     else if (i >= vec.size() / 2)
+    //         right.push_back(vec[i]);
+    // }
+    // Mergesort(left);
+    // Mergesort(right);
+    // vec.clear();
+    // while (left.size() > 0 && right.size() > 0)
+    // {
+    //     if (left.front() <= right.front())
+    //     {
+    //         vec.push_back(left.front());
+    //         left.erase(left.begin());
+    //     }
+    //     else
+    //     {
+    //         vec.push_back(right.front());
+    //         right.erase(right.begin());
+    //     }
+    // }
+    // while (left.size() > 0)
+    // {
+    //     vec.push_back(left.front());
+    //     left.erase(left.begin());
+    // }
+    // while (right.size() > 0)
+    // {
+    //     vec.push_back(right.front());
+    //     right.erase(right.begin());
+    // }
+    // std::merge(left.begin(), left.end(), right.begin(), right.end(), std::back_inserter(vec));
+// }
